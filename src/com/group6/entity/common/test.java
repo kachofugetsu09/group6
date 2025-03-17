071d0596c54c8cb5d9b11a4277118d50d4f10441
huashen <2494946808@qq.com>
2025-03-17 21:15:46 +0800

    test2

--- src/com/group6/entity/common/test.java
+++ src/com/group6/entity/common/test.java
@@ -3,5 +3,7 @@ package com.group6.entity.common;
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
+
 }
