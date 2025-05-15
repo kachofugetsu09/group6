415a0639611b581c595246d76757f6ca9881013a
刘昊普 <3050620856@qq.com>
2025-05-15 13:39:42 +0800

    更新GameController类以支持随机初始化瓦片位置和玩家起始位置；添加Treasure类以管理宝藏信息；修改Tile类以支持宝藏的设置和获取；更新GameBoard类以包含宝藏列表。

--- src/com/group6/entity/gameBoard/GameBoard.java
+++ src/com/group6/entity/gameBoard/GameBoard.java
@@ -1,26 +1,36 @@
 package com.group6.entity.gameBoard;
 
 import com.group6.entity.common.Tile;
+import com.group6.entity.common.Treasure;
 import com.group6.entity.player.Player;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Setter
 @Getter
 public class GameBoard {
     private List<Tile> tiles;
+    private List<Treasure> treasures;
     private List<Player> players;
 
-    System.out.println("GameBoard");
+    public GameBoard() {
+        tiles = new ArrayList<>();
+        treasures = new ArrayList<>();
+        players = new ArrayList<>();
+    }
 
-
-    public void getTiles(){
+    public List<Tile> getTiles() {
         return tiles;
     }
 
-    public void getPlayers(){
+    public List<Player> getPlayers() {
         return players;
+    }
+    
+    public List<Treasure> getTreasures() {
+        return treasures;
     }
 }
