91aece23b1f51c4ab45a0512c0fc3952e44f92e5
wangyuanzhe <2494946808@qq.com>
2025-05-26 14:57:56 +0800

    ci测试

--- src/com/group6/Main.java
+++ src/com/group6/Main.java
@@ -7,6 +7,7 @@ import javax.swing.SwingUtilities;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
+            int syntaxError = "这是一个编译错误"; // 类型不匹配
             GameFrame gameFrame = new GameFrame();
             gameFrame.setVisible(true);
         });
