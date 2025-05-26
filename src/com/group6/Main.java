711ad980f78a8ecbeafa130a2b4c868204800b95
wangyuanzhe <2494946808@qq.com>
2025-05-26 14:55:09 +0800

    审查测试131

--- src/com/group6/Main.java
+++ src/com/group6/Main.java
@@ -6,6 +6,8 @@ import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
+        // 故意引入编译错误来测试防护机制
+        int syntaxError = "这是一个编译错误"; // 类型不匹配
         SwingUtilities.invokeLater(() -> {
             GameFrame gameFrame = new GameFrame();
             gameFrame.setVisible(true);
